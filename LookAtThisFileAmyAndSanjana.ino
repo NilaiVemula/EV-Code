48dfea69d8426584d91b4d7f0472b9d27884edd3
NilaiVemula <nilaivemula@gmail.com>
2017-05-11 21:49:52 -0500

    Update LookAtThisFileAmyAndSanjana.ino

--- LookAtThisFileAmyAndSanjana.ino
+++ LookAtThisFileAmyAndSanjana.ino
@@ -57,6 +57,24 @@ Serial.print(encoder); //Prints encoder value//
 REAL_DISTANCE = ((encoder/600)*(2*3.14159*0.123825)) ///Conversion///
 Serial.println("Real Distance value (m)"); //Prints label//
 Serial.print(REAL_DISTANCE); //Prints distance value//
+//Controlling the motor based on the encoder value//
+  while(REAL_DISTANCE < change_speed_distance){ 
+    ////When the car is at a distance smaller than the changing_speed_location, the motor will go fast////
+    analogWrite(pinPwm, 200); //fast speed//
+    digitalWrite(pinDir, LOW);
+  }
+
+  while(REAL_DISTANCE >= change_speed_distance){ 
+    ////When the car is at a distance greater than the changing_speed_location, the motor will go slowly////
+    analogWrite(pinPwm, 50); //slow speed//
+    digitalWrite(pinDir, LOW);
+  }
+
+  if (REAL_DISTANCE >= final_distance){
+    ////When the car reaches (or exceeds) the final distance, pause for 10 seconds////
+    ////This should give you enough time to come to the car and turn it off////
+    delay(10000);
+  }
 }
 ///////////////////////////////////////////////////////////////////////////////////////////
 /// DO NOT EDIT///
@@ -70,22 +88,4 @@ encoder += digitalRead(encoder_a) != digitalRead(encoder_b) ? -1 : 1;
 }
 ///////////////////////////////////////////////////////////////////////////////////////////
 
-//Controlling the motor based on the encoder value//
-while(REAL_DISTANCE < change_speed_distance){ 
-  ////When the car is at a distance smaller than the changing_speed_location, the motor will go fast////
-  analogWrite(pinPwm, 200); //fast speed//
-  digitalWrite(pinDir, LOW);
-}
-
-while(REAL_DISTANCE >= change_speed_distance){ 
-  ////When the car is at a distance greater than the changing_speed_location, the motor will go slowly////
-  analogWrite(pinPwm, 50); //slow speed//
-  digitalWrite(pinDir, LOW);
-}
-
-if (REAL_DISTANCE >= final_distance){
-  ////When the car reaches (or exceeds) the final distance, pause for 10 seconds////
-  ////This should give you enough time to come to the car and turn it off////
-  delay(10000);
-}
 
