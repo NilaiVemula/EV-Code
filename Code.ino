8df256564a286ddab8c72a37a560cf6bd32e96a0
NilaiVemula <nilaivemula@gmail.com>
2017-05-11 21:26:03 -0500

    Update Code.ino

--- Code.ino
+++ Code.ino
@@ -1,109 +1,10 @@
-//ENCODER STUFF
-
-
-
-// Red - 5V
-// Black - GND
-const int encoder_a = 2; // use one white wire here
-const int encoder_b = 3; // use the other white wire here
-long encoder = 0;
-
-void setup() {
-Serial.begin(9600);
-pinMode(encoder_a, INPUT_PULLUP);
-pinMode(encoder_b, INPUT_PULLUP);
-
-attachInterrupt(0, encoderPinChangeA, CHANGE);
-attachInterrupt(1, encoderPinChangeB, CHANGE);
-}
-
-void loop() {
-Serial.println(encoder);
-}
-
-void encoderPinChangeA() {
-encoder += digitalRead(encoder_a) == digitalRead(encoder_b) ? -1 : 1;
-}
-
-void encoderPinChangeB() {
-encoder += digitalRead(encoder_a) != digitalRead(encoder_b) ? -1 : 1;
-}
-
-
-
-
-// MOTOR STUFF
-
-/*******************************************************************************
- * IO DEFINITION                                                                *
- *******************************************************************************/
-
-// PWM is connected to pin 3.
-const int pinPwm = 3;
-
-// DIR is connected to pin 2.
-const int pinDir = 2;
-
-
-
-/*******************************************************************************
- * PRIVATE GLOBAL VARIABLES                                                     *
- *******************************************************************************/
-
-// Speed of the motor.
-static int iSpeed = 0;
-
-// Acceleration of the motor.
-static int iAcc = 1;
-
-
-/*******************************************************************************
- * FUNCTIONS                                                                    *
- *******************************************************************************/
-
-// The setup routine runs once when you press reset.
-void setup() {                
-  // Initialize the PWM and DIR pins as digital outputs.
-  pinMode(pinPwm, OUTPUT);
-  pinMode(pinDir, OUTPUT);
-}
-
-
-
-// The loop routine runs over and over again forever.
-void loop() {
-  
-  // Control the motor according to the speed value.
-  // Positive speed value = CW,
-  // Negative speed value = CCW.
-  if (iSpeed >= 0) {
-    analogWrite(pinPwm, iSpeed);
-    digitalWrite(pinDir, LOW);
-  } 
-  else {
-    analogWrite(pinPwm, -iSpeed);
-    digitalWrite(pinDir, HIGH);
-  }
-  
-  // Increase/Decrease the speed according to the acceleration.
-  iSpeed += iAcc;
-  
-  // Change the acceleration sign when full speed is reached.
-  if ((iSpeed >= 255) || (iSpeed <= -255)) {
-    iAcc = -iAcc;
-  }
-  
-  
-  delay(10);
-}
-
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///FINAL CODE///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///Version 1 of Science Olympiad Electric Vehicle Code//////////////////////////////////////////////////////////////////////////////////////////////////////
+///White Station High School////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///Code Written by Nilai Vemula/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //VERY IMPORTANT SETUP INFO FIRST///////////////////////////////////////////////////////////////////////////////
@@ -170,18 +71,21 @@ encoder += digitalRead(encoder_a) != digitalRead(encoder_b) ? -1 : 1;
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 //Controlling the motor based on the encoder value//
-while(REAL_DISTANCE < change_speed_distance){ ////When the car is at a distance smaller than the changing_speed_location, the motor will go fast////
+while(REAL_DISTANCE < change_speed_distance){ 
+  ////When the car is at a distance smaller than the changing_speed_location, the motor will go fast////
   analogWrite(pinPwm, 200); //fast speed//
   digitalWrite(pinDir, LOW);
 }
 
-while(REAL_DISTANCE >= change_speed_distance){ ////When the car is at a distance greater than the changing_speed_location, the motor will go slowly////
+while(REAL_DISTANCE >= change_speed_distance){ 
+  ////When the car is at a distance greater than the changing_speed_location, the motor will go slowly////
   analogWrite(pinPwm, 50); //slow speed//
   digitalWrite(pinDir, LOW);
 }
 
-if (REAL_DISTANCE >= final_distance)
-{
+if (REAL_DISTANCE >= final_distance){
+  ////When the car reaches the final distance, pause for 10 seconds////
+  ////This should give you enough time to come to the car and turn it off////
   delay(10000);
 }
 
